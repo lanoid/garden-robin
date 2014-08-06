697f8815975aa199ef7c5850d6570315a449566e
Matthew Lane <matthew.lanoid@gmail.com>
2014-08-06 08:10:47 +0100

    Beginning transition to firebase backend

--- js/global.js
+++ js/global.js
@@ -1,4 +1,5 @@
-var gardenRobin = angular.module('gardenRobin', ['ngRoute','ngAnimate']);
+var gardenRobin = angular.module('gardenRobin', ['ngRoute','ngAnimate','firebase']);
+
 
 gardenRobin.config(function($routeProvider,$locationProvider){
 	$routeProvider
@@ -12,54 +13,70 @@ gardenRobin.config(function($routeProvider,$locationProvider){
 		});
 });
 
-gardenRobin.factory('Jobs', function($rootScope){
+gardenRobin.factory('Jobs', function($firebase) {
 	var Jobs = function Jobs(query){
 		this.query = query || {};
 		this.jobs = [];
-	};
-
-	Jobs.prototype.loadJobs = function(){
-		var jobs = this,
-			query = angular.copy(this.query);
-
-		dpd.jobs.get(query, function(result){
-			Array.prototype.push.apply(jobs.jobs, result);
-			$rootScope.$apply();
-		});
 	}
+
+	Jobs.prototype.loadJobs = function() {
+		console.log(this.query);
+		var ref = new Firebase('https://garden-robin.firebaseio.com/jobs')
+			.startAt(this.query)
+			.endAt(this.query)
+			.once('value', function(snap) {
+				console.log(snap.val());
+			});
+	}
+
 
 	return Jobs;
+
+
+	// var Jobs = function Jobs(query){
+	// 	this.query = query || {};
+	// 	this.jobs = [];
+	// };
+	//
+	// Jobs.prototype.loadJobs = function(path){
+	// 	var jobs = this,
+	// 		query = angular.copy(this.query);
+	//
+	// 	var ref = angularFireCollection(new Firebase(url + '/' + path));
+	// };
+	//
+	// return Jobs;
 });
 
-gardenRobin.factory('Photos', function($rootScope){
-	var Photos = function Photos(query){
-		this.query = query || {};
-		this.photos = [];
-	};
-
-	Photos.prototype.loadPhotos = function(){
-		var photos = this,
-			query = angular.copy(this.query);
-
-		dpd.photos.get(query, function(result){
-			Array.prototype.push.apply(photos.photos, result);
-			$rootScope.$apply();
-		});
-	}
-
-	return Photos;
-});
+// gardenRobin.factory('Photos', function($rootScope){
+// 	var Photos = function Photos(query){
+// 		this.query = query || {};
+// 		this.photos = [];
+// 	};
+//
+// 	Photos.prototype.loadPhotos = function(){
+// 		var photos = this,
+// 			query = angular.copy(this.query);
+//
+// 		dpd.photos.get(query, function(result){
+// 			Array.prototype.push.apply(photos.photos, result);
+// 			$rootScope.$apply();
+// 		});
+// 	};
+//
+// 	return Photos;
+// });
 
 gardenRobin.directive('svgClick', function($location){
 	return {
 		link : function($scope, iElement, iAttrs, controller){
 			iElement.bind('click', function(){
 				$scope.$apply(function() {
-				  $location.path($(iElement).data('query'));
+					$location.path($(iElement).data('query'));
 				});
 			});
 		}
-	}
+	};
 });
 
 gardenRobin.directive('svgHover', function(){
@@ -72,7 +89,7 @@ gardenRobin.directive('svgHover', function(){
 				$('.months a.' + $(iElement).attr('class')).removeClass('hovering');
 			});
 		}
-	}
+	};
 });
 
 gardenRobin.directive('monthHover', function(){
@@ -85,7 +102,7 @@ gardenRobin.directive('monthHover', function(){
 				$('.months path.' + $(iElement).attr('class'))[0].classList.remove('hovering');
 			});
 		}
-	}
+	};
 });
 
 gardenRobin.directive('date', function(){
@@ -93,21 +110,19 @@ gardenRobin.directive('date', function(){
 		link : function($scope, iElement, iAttrs, controller){
 			$scope.month = new Date();
 		}
-	}
+	};
 });
 
 gardenRobin.controller('jobsCtrl', function($scope,$routeParams,Jobs){
+	console.log($routeParams);
 	var jobs = new Jobs({month:$routeParams.month});
-
-	jobs.loadJobs();
-	$scope.jobs = jobs.jobs;
-	$scope.jobs.open = true;
+	jobs.loadJobs()
 });
 
-gardenRobin.controller('photosCtrl', function($scope,$routeParams,Photos){
-	var photos = new Photos({season:$routeParams.season});
-
-	photos.loadPhotos();
-	$scope.photos = photos.photos;
-	$scope.photos.open = true;
-});
+// gardenRobin.controller('photosCtrl', function($scope,$routeParams,Photos){
+// 	var photos = new Photos({season:$routeParams.season});
+//
+// 	photos.loadPhotos();
+// 	$scope.photos = photos.photos;
+// 	$scope.photos.open = true;
+// });
